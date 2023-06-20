cb9ee7b0fadd6f969aee2f8786f37dcc92f8c7ca
PopescuAndrei09 <popescuandrei93251@yahoo.com>
2023-06-20 01:30:07 -0700

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
 'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Key': 'ed55e878d0msh0aa1cf2b4e0e701p1b9c40jsn1ae176818d78',
-    'X-RapidAPI-Host': 'bing-web-search1.p.rapidapi.com'
+    'X-RapidAPI-Key':  process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': ' process.env.REACT_APP_RAPID_API_KEY'
 };
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+const baseUrl = 'process.env.REACT_APP_RAPID_API_KEY';
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
 export const cryptoNewsApi = createApi({
